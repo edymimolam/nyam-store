951be9cb48bf09f22d2afdce475cddd7b28ecc85
edymimolam <edymimolam@gmail.com>
2020-08-27 19:56:10 +0300

    Updates

---
+++ precache-manifest.712439221d2cac0d950078ad12bcac45.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e98d4d7aed61ea3ab03304dbc8e6e85",
+    "url": "/nyam-store/index.html"
+  },
+  {
+    "revision": "cb6caa83f34a53ac1f4f",
+    "url": "/nyam-store/static/js/2.e0b463ea.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/nyam-store/static/js/2.e0b463ea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "467da96c999f09f207e1",
+    "url": "/nyam-store/static/js/main.eb795539.chunk.js"
+  },
+  {
+    "revision": "10a381f3789377d0fefd",
+    "url": "/nyam-store/static/js/runtime-main.0da3796d.js"
+  },
+  {
+    "revision": "44bde8744ccc0de7995a23b0e82dd281",
+    "url": "/nyam-store/static/media/Exo2.0-Thin.44bde874.otf"
+  },
+  {
+    "revision": "79d68011797f7124dd17f3a480a7a01f",
+    "url": "/nyam-store/static/media/background-pattern.79d68011.png"
+  },
+  {
+    "revision": "3fff01eef09dad618445c36f3fd3eb2c",
+    "url": "/nyam-store/static/media/cat-photo.3fff01ee.png"
+  }
+]);
